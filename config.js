f59d5f5adce9ac89db8129b7f1f3c4ec699e9160
Alberto Arias Azcona <maug.thief@gmail.com>
2017-11-17 11:36:51 -0600

    Mueve configuracion a pruebas

--- config.js
+++ config.js
@@ -28,14 +28,14 @@ module.exports = {
   // ////////////////////////////////////////////////////////////////////////////////////////
 
   ApiErp: 'https://ws.compusoluciones.com/IntelisisPruebas/',
-  // ApiErp: 'http://localhost:54228/',
+  // // ApiErp: 'http://localhost:54228/',
   AccessControlAllowOrigin: '*',
   connectionPool: mySqlModel.createPool({
     connectionLimit: 10,
-    host: '10.3.102.52',
+    host: '10.3.102.70',
     user: 'root',
     password: 'Inn0vaci0n',
-    database: 'clicksuscribeDev',
+    database: 'clicksuscribeTrial',
     dateStrings: 'date',
     debug: false,
   }),
