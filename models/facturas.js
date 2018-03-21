3601c27b86353e51f7e31ae8d934650c57f1f093
Héctor De La Cruz [GDL] <hcruz@compusoluciones.com>
2018-03-21 12:45:01 -0600

    Se realizan cambios a querys para que tome la fecha fin correctamente de contratos autodesk al facturar

--- models/facturas.js
+++ models/facturas.js
@@ -20,26 +20,31 @@ facturas.generarBajoConsumo = () => facturas.obtenerBajoConsumoPendientes().then
 // Consulta en la base de datos del MarketPlace todas los pedidos pendientes de facturar, es decir Facturado = 0 y que el IdEstatusPedido sea 2, 4 o 5
 facturas.obtenerPendientes = () => help.d$().query(`
   SELECT DISTINCT
-    P.IdPedido, Distribuidor.IdERP AS Cliente, IFNULL(Distribuidor.Credito, 0) Credito, UsuarioFinal.NombreEmpresa AS Proyecto, F.UEN, P.MonedaPago, P.TipoCambio, P.IdFormaPago,
-    fn_CalcularTotalPedido(P.IdPedido) AS Total,
-    fn_CalcularIVA(fn_CalcularTotalPedido(P.IdPedido), Distribuidor.ZonaImpuesto) AS IVA, P.FechaFin AS Vencimiento,
-    CASE WHEN P.IdFabricante = 1 THEN Distribuidor.AgenteMicrosoft WHEN P.IdFabricante = 2 THEN Distribuidor.AgenteAutodesk ELSE NULL END AS Agente
+  P.IdPedido, Distribuidor.IdERP AS Cliente, IFNULL(Distribuidor.Credito, 0) Credito, UsuarioFinal.NombreEmpresa AS Proyecto, F.UEN, P.MonedaPago, P.TipoCambio, P.IdFormaPago,
+  fn_CalcularTotalPedido(P.IdPedido) AS Total,
+  fn_CalcularIVA(fn_CalcularTotalPedido(P.IdPedido), Distribuidor.ZonaImpuesto) AS IVA,
+  CASE WHEN P.IdFabricante = 1 THEN P.FechaFin WHEN P.IdFabricante = 2 THEN contrato.FechaFin END AS Vencimiento,
+  CASE WHEN P.IdFabricante = 1 THEN Distribuidor.AgenteMicrosoft WHEN P.IdFabricante = 2 THEN Distribuidor.AgenteAutodesk ELSE NULL END AS Agente
   FROM traPedidos P
+  LEFT JOIN traContratoAutodesk contrato ON contrato.IdContrato = P.IdContrato AND contrato.Activo = 1
   INNER JOIN traEmpresas Distribuidor ON Distribuidor.IdEmpresa = P.IdEmpresaDistribuidor
   INNER JOIN traEmpresas UsuarioFinal ON UsuarioFinal.IdEmpresa = P.IdEmpresaUsuarioFinal
   INNER JOIN traFabricantes F ON F.IdFabricante = P.IdFabricante
   INNER JOIN traPedidoDetalles PD ON PD.IdPedido = P.IdPedido AND (PD.Activo = 1 OR PD.PorCancelar = 1) AND PD.PedidoAFabricante = 1
   INNER JOIN traProductos Pro ON Pro.IdProducto = PD.IdProducto AND Pro.IdTipoProducto != 3
   WHERE P.Facturado = 0 AND P.IdEstatusPedido IN (2, 3, 4, 5) AND Distribuidor.IdERP IS NOT NULL AND P.PedidoImportado IS NULL
-  AND UsuarioFinal.NombreEmpresa IS NOT NULL AND F.UEN IS NOT NULL AND P.MonedaPago IS NOT NULL AND P.TipoCambio IS NOT NULL AND P.FechaFin IS NOT NULL;`);
+  AND UsuarioFinal.NombreEmpresa IS NOT NULL AND F.UEN IS NOT NULL AND P.MonedaPago IS NOT NULL AND P.TipoCambio IS NOT NULL
+  AND CASE WHEN P.IdFabricante = 1 THEN P.FechaFin IS NOT NULL WHEN P.IdFabricante = 2 THEN contrato.FechaFin IS NOT NULL END;`);
 
 facturas.obtenerBajoConsumoPendientes = () => help.d$().query(`
   SELECT DISTINCT
     P.IdPedido, Distribuidor.IdERP AS Cliente, IFNULL(Distribuidor.Credito, 0) Credito, UsuarioFinal.NombreEmpresa AS Proyecto, F.UEN, P.MonedaPago, P.TipoCambio, P.IdFormaPago,
     fn_CalcularTotalPedido(P.IdPedido) AS Total,
-    fn_CalcularIVA(fn_CalcularTotalPedido(P.IdPedido), Distribuidor.ZonaImpuesto) AS IVA, P.FechaFin AS Vencimiento,
+    fn_CalcularIVA(fn_CalcularTotalPedido(P.IdPedido), Distribuidor.ZonaImpuesto) AS IVA,
+    CASE WHEN P.IdFabricante = 1 THEN P.FechaFin WHEN P.IdFabricante = 2 THEN contrato.FechaFin END AS Vencimiento,
     CASE WHEN P.IdFabricante = 1 THEN Distribuidor.AgenteMicrosoft WHEN P.IdFabricante = 2 THEN Distribuidor.AgenteAutodesk ELSE NULL END AS Agente
   FROM traPedidos P
+  LEFT JOIN traContratoAutodesk contrato ON contrato.IdContrato = P.IdContrato AND contrato.Activo = 1
   INNER JOIN traEmpresas Distribuidor ON Distribuidor.IdEmpresa = P.IdEmpresaDistribuidor
   INNER JOIN traEmpresas UsuarioFinal ON UsuarioFinal.IdEmpresa = P.IdEmpresaUsuarioFinal
   INNER JOIN traFabricantes F ON F.IdFabricante = P.IdFabricante
