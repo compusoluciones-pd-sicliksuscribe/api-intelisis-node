8035467bc15111f653f139c1a2e650d6477c689f
Alejandro Cruz <jorcruz@compusoluciones.com>
2018-05-29 11:14:28 -0500

    Cambios para la integración de los anticipos a node Intelisis.

--- application/bill-prepaids/index.js
+++ application/bill-prepaids/index.js
@@ -1,20 +1,21 @@
 const applyOrderPrepaid = require('../intelisis/bill-prepaids/create-order');
 const applyOrderDetailsPrepaid = require('../intelisis/bill-prepaids/create-order-details');
 
-const extractDetailsById = orders => {
-  applyOrderPrepaid(orders)
-  .then(erpResponse => {
-    const id = erpResponse[0].ID;
-    const updateDetails = orders.detalles.map((detail, index) => {
-      const renglonId = index + 1;
-      const renglon = renglonId * 2048;
-      const detailWithId = Object.assign({}, detail, { ID: id, renglon, renglonId });
-      return applyOrderDetailsPrepaid(detailWithId);
-    });
-    return Promise.all(updateDetails);
-  })
-  .then(erpResponse => applyOrderDetailsPrepaid(erpResponse, orders));
+const extractOrderDetailsById = (erpResponse, orders) => {
+  const id = erpResponse[0].ID;
+  const updateDetails = orders.detalles.map((detail, index) => {
+    const renglonId = index + 1;
+    const renglon = renglonId * 2048;
+    const detailWithId = Object.assign({}, detail, { ID: id, renglon, renglonId });
+    return applyOrderDetailsPrepaid(detailWithId);
+  });
+  return Promise.all(updateDetails);
 };
+
+const extractDetailsById = orders => (
+  applyOrderPrepaid(orders)
+  .then(erpResponse => extractOrderDetailsById(erpResponse, orders))
+);
 
 const billPrepaids = orders => (
 extractDetailsById(orders)
