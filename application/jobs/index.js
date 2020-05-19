54e123be289a37c5a6c74211bb237d673732601c
Marco Hernandez <mhernandez@compusoluciones.com>
2020-05-19 09:54:09 -0500

    Job de facturación de pedidos por Mov pedido aws

--- application/jobs/index.js
+++ application/jobs/index.js
@@ -1,10 +1,12 @@
 const billMsOrders = require('./billMsOrders');
+const billAWSOrders = require('./billAWSOrders');
 const actualizar = require('./actualizar');
 
 
 const start = () => {
   billMsOrders.start();
   actualizar.start();
+  billAWSOrders.start();
 };
 module.exports = {
   start,
