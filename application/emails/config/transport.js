75a78b82330453da7930c54f7b947d42a310b234
Mario Deras <josedera@compusoluciones.com>
2021-11-05 15:54:59 -0600

    Regresa contraseña

--- application/emails/config/transport.js
+++ application/emails/config/transport.js
@@ -5,7 +5,7 @@ emailerConfig.nodemailerTransport = {
   port: '587',
   auth: {
     user: 'clicksuscribe@compusoluciones.com',
-    pass: '8f&lX6m1F8TGq5#',
+    pass: 'lzxmtdckymfwwccx',
   },
   secureConnection: false,
   secure: false,
