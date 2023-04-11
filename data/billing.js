e5c2c139a7833d57bb267befb5d7a4c6b104186d
Marco Antonio Hernandez Ramirez <mhernandez@compusoluciones.com>
2023-04-11 14:07:17 -0600

    Se agregan campos a considerar

--- data/billing.js
+++ data/billing.js
@@ -173,51 +173,54 @@ WHERE
 
 billing.getOrdersToBillAzurePlan = () => help.d$().query(`
 SELECT DISTINCT
-  P.IdPedido,
-  P.IdPrimerPedido,
-  Distribuidor.IdERP AS Cliente,
-  IFNULL(Distribuidor.Credito, 0) Credito,
-  UsuarioFinal.NombreEmpresa AS Proyecto,
-  F.UEN,
-  P.MonedaPago,
-  P.TipoCambio AS TipoCambio,
-  P.IdFormaPago,
-  FN_CALCULARTOTALPEDIDO(P.IdPedido) AS Total,
-  FN_CALCULARIVA(FN_CALCULARTOTALPEDIDO(P.IdPedido),
-          Distribuidor.ZonaImpuesto) AS IVA,
-  P.FechaFin AS Vencimiento,
-  Distribuidor.AgenteMicrosoft AS Agente,
-  '' AS EsquemaRenovacion,
-  '' AS Observaciones,
-  0 AS SinCredito,
-  '' AS FormaPago
+    P.IdPedido,
+    P.IdPrimerPedido,
+    Distribuidor.IdERP AS Cliente,
+    IFNULL(Distribuidor.Credito, 0) Credito,
+    UsuarioFinal.NombreEmpresa AS Proyecto,
+    P.IdFabricante,
+    UsuarioFinal.RFC,
+    UsuarioFinal.DominioMicrosoftUF,
+    F.UEN,
+    P.MonedaPago,
+    P.TipoCambio AS TipoCambio,
+    P.IdFormaPago,
+    FN_CALCULARTOTALPEDIDO(P.IdPedido) AS Total,
+    FN_CALCULARIVA(FN_CALCULARTOTALPEDIDO(P.IdPedido),
+            Distribuidor.ZonaImpuesto) AS IVA,
+    P.FechaFin AS Vencimiento,
+    Distribuidor.AgenteMicrosoft AS Agente,
+    '' AS EsquemaRenovacion,
+    '' AS Observaciones,
+    0 AS SinCredito,
+    '' AS FormaPago
 FROM
-  traPedidos P
-      INNER JOIN
-  traEmpresas Distribuidor ON Distribuidor.IdEmpresa = P.IdEmpresaDistribuidor
-      INNER JOIN
-  traEmpresas UsuarioFinal ON UsuarioFinal.IdEmpresa = P.IdEmpresaUsuarioFinal
-      INNER JOIN
-  traFabricantes F ON F.IdFabricante = P.IdFabricante
-      INNER JOIN
-  traPedidoDetalles PD ON PD.IdPedido = P.IdPedido
-      AND PD.PedidoAFabricante = 1
-      INNER JOIN
-  traProductos Pro ON Pro.IdProducto = PD.IdProducto
+    traPedidos P
+        INNER JOIN
+    traEmpresas Distribuidor ON Distribuidor.IdEmpresa = P.IdEmpresaDistribuidor
+        INNER JOIN
+    traEmpresas UsuarioFinal ON UsuarioFinal.IdEmpresa = P.IdEmpresaUsuarioFinal
+        INNER JOIN
+    traFabricantes F ON F.IdFabricante = P.IdFabricante
+        INNER JOIN
+    traPedidoDetalles PD ON PD.IdPedido = P.IdPedido
+        AND PD.PedidoAFabricante = 1
+        INNER JOIN
+    traProductos Pro ON Pro.IdProducto = PD.IdProducto
 WHERE
-  P.Facturado = 0
-    AND P.IdEstatusPedido NOT IN (1, 6)
-    AND Distribuidor.IdERP IS NOT NULL
-    AND P.PedidoImportado IS NULL
-    AND UsuarioFinal.NombreEmpresa IS NOT NULL
-    AND F.UEN IS NOT NULL
-    AND P.MonedaPago IS NOT NULL
-    AND P.TipoCambio IS NOT NULL
-    AND P.Facturado != 1
-    AND P.IdEsquemaRenovacion IN (?)
-    AND P.FechaFin = Date_format(now(),'%Y-%m-06')
-    AND PD.PrecioUnitario > 0.1
-    LIMIT 20;`, [AZURE_PLAN]);
+    P.Facturado = 0
+        AND P.IdEstatusPedido NOT IN (1, 6)
+        AND Distribuidor.IdERP IS NOT NULL
+        AND P.PedidoImportado IS NULL
+        AND UsuarioFinal.NombreEmpresa IS NOT NULL
+        AND F.UEN IS NOT NULL
+        AND P.MonedaPago IS NOT NULL
+        AND P.TipoCambio IS NOT NULL
+        AND P.Facturado != 1
+        AND P.IdEsquemaRenovacion IN (?)
+        AND P.FechaFin = Date_format(now(),'%Y-%m-06')
+        AND PD.PrecioUnitario > 0.1
+        LIMIT 20;`, [AZURE_PLAN]);
 
 billing.selectPendingOrderDetail = IdPedido => help.d$().query(`
 SELECT 
