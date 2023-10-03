ac279df25dee7812c34c2e76d3d633773b2d6042
Jesús Arreola [GDL] <jarreola@compusoluciones.com>
2023-10-02 18:27:49 -0600

    Se agregan helpers

--- application/intelisis/get-openpay-info/index.js
+++ application/intelisis/get-openpay-info/index.js
@@ -2,17 +2,17 @@ const moment = require('moment');
 const ordersData = require('../../../data/orders');
 const paymentTypes = require('../../../helpers/enums/auxiliariesOpenpay');
 const array = require('joi/lib/types/array');
-
-
+const { FIRST_ITEM } = paymentTypes;
 
 const openpayInfo = async (paymentFormat, order) => {
     let resInfo = '';
     let paymentMethod = '';
+    let stringId = 'id:';
     const errorMssg = 'Pago no realizado en la plataforma.';
     if (paymentFormat == paymentTypes.CARD_PAYMENT_ID) {
         resInfo = await ordersData.getOpenpayCCInfo(order);
-        if (resInfo.data[0]) {
-            let descriptionArray = new String(resInfo.data[0].des);
+        if (resInfo.data[FIRST_ITEM]) {
+            let descriptionArray = new String(resInfo.data[FIRST_ITEM].des);
             paymentMethod = `(${descriptionArray[paymentTypes.CARD_PAYMENT_ID]})${paymentTypes.CARD}`;
         } else {
             return errorMssg;
@@ -20,13 +20,13 @@ const openpayInfo = async (paymentFormat, order) => {
     } else {
         resInfo = await ordersData.getOpenpaySpeiInfo(order).catch(error => console.log(error));
         paymentMethod = paymentTypes.SPEI;
-        if (resInfo.data[0]) {
+        if (resInfo.data[FIRST_ITEM]) {
             paymentMethod = paymentTypes.SPEI;
         } else {
             return errorMssg;
         }
     }
-    return resInfo.data[0] ? `id:${resInfo.data[0].idOP}, ${paymentMethod}, ${moment(resInfo.data[0].date).format('DD/MM/YYYY')}` : errorMssg;
+    return resInfo.data[FIRST_ITEM] ? `${stringId}${resInfo.data[FIRST_ITEM].idOP}, ${paymentMethod}, ${moment(resInfo.data[FIRST_ITEM].date).format('DD/MM/YYYY')}` : errorMssg;
 };
 
 module.exports = openpayInfo;
